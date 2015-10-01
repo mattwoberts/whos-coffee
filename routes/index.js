42ffa1e878f454b8142b2a2fe6e4dc981bc3796c
Matt Roberts <roberts.mattroberts@gmail.com>
2015-10-01 16:08:01 +0100

    It works. Coffee is mine.

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,56 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+var moment = require('moment');
 
 
 
 var Datastore = require('nedb');
-var db = new Datastore({ filename: '../datafile2', autoload: true });
-
+var historydb = new Datastore({ filename: 'history.dat', autoload: true });
+var playersdb = new Datastore({ filename: 'players.dat', autoload: true });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+  // Find the last coffee buyer.
+  historydb.find({}).sort({ when: -1 }).limit(1).exec(function(err,docs) {
+    console.log('Hey, found a doc', docs[0], err, docs.length);
 
-  db.find({}, function (err, docs) {
-    if (docs.length == 0) {
-      console.log('No existing documents in the database...');
-      var coffee = {who:'Si', when:new Date(2015,8,24)};
-      db.insert(coffee);
-    }
+    var last = docs[0];
 
-    db.find({}).sort({ when: -1 }).limit(1).exec(function(err,docs) {
-      console.log('Hey, found a doc', docs[0], err, docs.length);
-      res.render('index',{last: docs[0], title:'Poop'});
+    // Now find who's next up:
+
+    playersdb.find({name: {$ne:last.who}}).exec(function(err,docs) {
+      var next = docs[0];
+
+      var emailHash = crypto.createHash('md5').update(next.email).digest('hex');
+      var gravatar = 'http://www.gravatar.com/avatar/' + emailHash;
+
+      res.render('index',
+        {
+          last: last,
+          last_date: moment(last.when).fromNow(),
+          next: next, 
+          title:'Coffee',
+          gravatar:gravatar
+        });
     });
-
   });
+});
 
+
+router.post('/', function(req,res, next) {
+
+  console.log(req.param('turn'));
+
+  // Do some db things...
+
+  var record = {who:req.param('turn'),when:new Date()};
+  historydb.insert(record, function (err, newDoc) {   // Callback is optional
+    // newDoc is the newly inserted document, including its _id
+    // newDoc has no key called notToBeSaved since its value was undefined
+    res.redirect('/');
+  });
 
 
 });
