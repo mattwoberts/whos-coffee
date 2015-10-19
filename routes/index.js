664e5b36545eba8606f90c1b933a559a44394ba6
Matt Roberts <roberts.mattroberts@gmail.com>
2015-10-19 10:19:37 +0100

    Seed the missing data if needed

--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,42 @@ var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
 var moment = require('moment');
-
-
-
 var Datastore = require('nedb');
-var historydb = new Datastore({ filename: 'history.dat', autoload: true });
-var playersdb = new Datastore({ filename: 'players.dat', autoload: true });
+
+
+// Set up the database.
+var db ={};
+db.players = new Datastore({ filename: 'players.dat', autoload: true });
+db.history = new Datastore({ filename: 'history.dat', autoload: true });
+
+
+// Check for some users...
+db.players.find({}).exec(function(err,docs) {
+  if (!docs.length) {
+    console.log('No players, time to init the database');
+    db.players.insert({name:'Si', email:'simon.phillipson@gmail.com'});
+    db.players.insert({name:'Matt', email:'roberts.mattroberts@gmail.com'});
+    db.history.insert({who:'Si',when: new Date()});
+  } else {
+    console.log('Players found');
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   // Find the last coffee buyer.
-  historydb.find({}).sort({ when: -1 }).limit(1).exec(function(err,docs) {
-    console.log('Hey, found a doc', docs[0], err, docs.length);
+  db.history.find({}).sort({ when: -1 }).limit(1).exec(function(err,docs) {
 
     var last = docs[0];
 
-    // Now find who's next up:
-
-    console.log('looking for the player..');
-
-    playersdb.find({name: {$ne:last.who}}).exec(function(err,docs) {
+    db.players.find({name: {$ne:last.who}}).exec(function(err,docs) {
       var next = docs[0];
 
       console.log('found a player..',next);
 
       var emailHash = crypto.createHash('md5').update(next.email).digest('hex');
       var gravatar = 'http://www.gravatar.com/avatar/' + emailHash;
-
-      console.log('rendering the view...');
 
       res.render('index',
         {
@@ -47,12 +54,8 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req,res, next) {
 
-  console.log(req.param('turn'));
-
-  // Do some db things...
-
   var record = {who:req.param('turn'),when:new Date()};
-  historydb.insert(record, function (err, newDoc) {   // Callback is optional
+  db.history.insert(record, function (err, newDoc) {   // Callback is optional
     // newDoc is the newly inserted document, including its _id
     // newDoc has no key called notToBeSaved since its value was undefined
     res.redirect('/');
